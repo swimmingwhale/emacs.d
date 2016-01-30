8eda0c4b08e7acadc6d20e65ef8d5b26954c97f1
zhangqiang <zxc4481@163.com>
2016-01-30 09:55:41 +0800

    add by swimmingwhale

--- init.el
+++ init.el
@@ -92,6 +92,17 @@ c-basic-offset 4)
 
 (setq frame-title-format '("" buffer-file-name "@emacs" ));在标题栏显示buffer名称
 
+;;; Lisp (SLIME) interaction
+(setq inferior-lisp-program "/usr/bin/clisp")
+(add-to-list 'load-path "~/.slime")
+(require 'slime)
+(slime-setup)
+
+(global-font-lock-mode t)
+(show-paren-mode 1)
+(add-hook 'lisp-mode-hook '(lambda ()
+(local-set-key (kbd "RET") 'newline-and-indent)))
+
 
 
 ;启动最大化
