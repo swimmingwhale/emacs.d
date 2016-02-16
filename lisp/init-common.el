57064d5728c57e777e72d9253f56c6d2a1872d09
zhangqiang <zxc4481@163.com>
2016-02-16 10:20:42 +0800

    add by swimmingwhale

--- lisp/init-common.el
+++ lisp/init-common.el
@@ -2,13 +2,18 @@
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0) 
-(setq make-backup-files 0);取消定时备份
-(setq backup-by-copying t);取消emacs修改文件先备份为~后缀文件
-(fset 'yes-or-no-p 'y-or-n-p);以 y/n代表 yes/no
-(column-number-mode t);显示列号
-(show-paren-mode t);显示括号匹配
-(setq mouse-yank-at-point t);支持中键粘贴
-(setq frame-title-format '("" buffer-file-name "@emacs" ));在标题栏显示buffer名称
+;将备份文件放到制定目录
+(setq backup-directory-alist (quote (("." . "~/.backups"))))
+;以 y/n代表 yes/no
+(fset 'yes-or-no-p 'y-or-n-p)
+;显示列号
+(column-number-mode t)
+;显示括号匹配
+(show-paren-mode t)
+;支持中键粘贴
+(setq mouse-yank-at-point t)
+;在标题栏显示buffer名称
+(setq frame-title-format '("" buffer-file-name "@emacs" ))
 ; 设置默认tab宽度为4
 (setq tab-width 4 indent-tabs-mode t c-basic-offset 4)
 ; 按下C-x k立即关闭掉当前的buffer
