325cb5317f21962264d8e1325ff2873f2649785f
zhangqiang <zxc4481@163.com>
2016-02-24 17:15:17 +0800

    add by swimmingwhale

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -1,8 +1,21 @@
+;window-number模式
 (require 'window-numbering)
 (window-numbering-mode 1)
-;恢复到之前的窗口状态
 (winner-mode 1)
-;; copied from http://puntoblogspot.blogspot.com/2011/05/undo-layouts-in-emacs.html
+
+;恢复到之前的窗口状态
 (global-set-key (kbd "C-x 4 u") 'winner-undo)
 (global-set-key (kbd "C-x 4 r") 'winner-redo)
+
+ (defun split-window-4() 
+ "Splite window into 4 sub-window"
+ (interactive) 
+ (if (= 1 (length (window-list))) 
+     (progn (split-window-vertically) 
+        (split-window-horizontally) 
+        ) 
+   ) 
+ ) 
+(global-set-key (kbd "C-x 4 4") 'split-window-4)
+
 (provide 'init-windows)
