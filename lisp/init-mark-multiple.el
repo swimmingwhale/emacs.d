dbc09585b7246d3983420b5d2ef1bec7c2206bf6
zhangqiang <zxc4481@163.com>
2016-02-25 11:11:54 +0800

    add by swimmingwhale

---
+++ lisp/init-mark-multiple.el
@@ -0,0 +1,8 @@
+(add-to-list 'load-path "~/.emacs.d/package/mark-multiple")
+(require 'mark-more-like-this)  
+(global-set-key (kbd "C-<") 'mark-previous-like-this)  
+(global-set-key (kbd "C->") 'mark-next-like-this)  
+(global-set-key (kbd "C-M-m") 'mark-more-like-this) ; like the other two, but takes an argument (negative is previous)  
+(global-set-key (kbd "C-*") 'mark-all-like-this) 
+
+(provide 'init-mark-multiple)
