91c0ceb1e94706e22f892e2b2cc4d1dd7071aa7e
swimmingwhale <kupposhadow@gmail.com>
2016-05-03 10:51:10 +0800

    add by swimmingwhale

--- lisp/init-gtags.el
+++ lisp/init-gtags.el
@@ -1,7 +1,6 @@
-(require 'gtags)
-;get into gtags-mode whenever you get into php-mode
-(setq gtags-suggested-key-mapping t)
+(autoload 'gtags-mode "gtags" "" t)
 (setq php-mode-hook
       '(lambda ()
      (gtags-mode 1)))
+(setq gtags-suggested-key-mapping t)
 (provide 'init-gtags)
