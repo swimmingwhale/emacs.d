a3a979145b0780d996cb2ba154138e2ad671342f
swimmingwhale <kupposhadow@gmail.com>
2016-05-15 19:52:07 +0800

    add by swimmingwhale

--- lisp/init-org-mode.el
+++ lisp/init-org-mode.el
@@ -12,4 +12,35 @@
      ))
 
 (setq org-todo-keyword-faces '(("DELETE" . "yellow")))
+
+;publish-project
+(setq org-publish-project-alist
+      '(
+
+  ("org-ianbarton"
+          ;; Path to your org files.
+          :base-directory "/work/myproject/org/"
+          :base-extension "org"
+
+          ;; Path to your Jekyll project.
+          :publishing-directory "/work/myproject/jekyll/"
+          :recursive t
+          :publishing-function org-html-publish-to-html
+          :headline-levels 4 
+          :html-extension "html"
+          :body-only t ;; Only export section between <body> </body>
+    )
+
+
+    ("org-static-ian"
+          :base-directory "/work/myproject/org/"
+          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|php"
+          :publishing-directory "/work/project/"
+          :recursive t
+          :publishing-function org-publish-attachment)
+
+    ("ian" :components ("org-ianbarton" "org-static-ian"))
+
+))
+
 (provide 'init-org-mode)
