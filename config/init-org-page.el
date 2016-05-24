4d7ee1449d18b475ec08939413a0fa87faaf48ee
swimmingwhale <kupposhadow@gmail.com>
2016-05-24 15:18:43 +0800

    add by swimmingwhale

--- config/init-org-page.el
+++ config/init-org-page.el
@@ -1,11 +1,11 @@
 ;;; the following is only needed if you install org-page manually
 (require 'org-page)
-(setq op/repository-directory "~/test")
+(setq op/repository-directory "~/work/swimmingwhale.github.io")
 (setq op/site-domain "http://www.kuppobook.com/")
 ;;; for commenting, you can choose either disqus or duoshuo
 (setq op/personal-disqus-shortname "your_disqus_shortname")
 (setq op/personal-duoshuo-shortname "your_duoshuo_shortname")
 ;;; the configuration below are optional
 (setq op/personal-google-analytics-id "your_google_analytics_id")
-(setq op/theme 'mdo)
+(setq op/theme 'mmn)
 (provide 'init-org-page)
