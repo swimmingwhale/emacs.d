1179a0d28854736dec031c88c9cf81145b2fea41
swimmingwhale <kupposhadow@gmail.com>
2016-05-25 23:36:37 +0800

    Revert "add by swimmingwhale"
    This reverts commit d7a8691f3eda4e1d8d1580c89fc02652f10f86ef.

--- config/init-org-page.el
+++ config/init-org-page.el
@@ -5,7 +5,11 @@
 (setq op/site-main-title "木马牛")
 (setq op/site-sub-title "等咱有了钱，买两碗豆浆，一碗爱喝不喝，一碗就是不喝！")
 (setq op/personal-github-link "https://github.com/swimmingwhale")
-(setq op/personal-disqus-shortname "swimmingwhale")
+(setq op/personal-disqus-shortname "kuppo")
+;;; for commenting, you can choose either disqus or duoshuo
+(setq op/personal-disqus-shortname "your_disqus_shortname")
+(setq op/personal-duoshuo-shortname "your_duoshuo_shortname")
+;;; the configuration below are optional
 (setq op/personal-google-analytics-id "your_google_analytics_id")
 (setq op/theme 'mmn)
 (provide 'init-org-page)
